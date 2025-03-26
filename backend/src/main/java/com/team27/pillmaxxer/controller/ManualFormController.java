6fdbc2d0edf51ea80c2403e1f0693b55559b05d9
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-03-26 16:20:20 -0400

    pulled main

---
+++ backend/src/main/java/com/team27/pillmaxxer/controller/ManualFormController.java
@@ -0,0 +1,10 @@
+package com.team27.pillmaxxer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/manual-form")
+public class ManualFormController {
+
+}
---
+++ backend/src/main/java/com/team27/pillmaxxer/dto/ManualFormRequest.java
@@ -0,0 +1,5 @@
+package com.team27.pillmaxxer.dto;
+
+public class ManualFormRequest {
+
+}
