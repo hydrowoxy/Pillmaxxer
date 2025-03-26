e2c10679c798607158175b8025cd887c23fa5f09
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-03-26 16:01:46 -0400

    set up controller and dto for manual form entry

--- backend/src/main/java/com/team27/pillmaxxer/controller/DrugInteractionController_DDInter.java
+++ backend/src/main/java/com/team27/pillmaxxer/controller/DrugInteractionController_DDInter.java
@@ -1,11 +1,13 @@
 package com.team27.pillmaxxer.controller;
 
-import com.team27.pillmaxxer.service.DrugInteractionService_DDInter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.team27.pillmaxxer.service.DrugInteractionService_DDInter;
+
 @RestController
 @RequestMapping("/api/ddi")
 public class DrugInteractionController_DDInter {
