13136261378db1e5709f84305dab91ded847015e
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-03-26 22:12:59 -0400

    dto

--- backend/src/main/java/com/team27/pillmaxxer/dto/ManualFormRequest.java
+++ backend/src/main/java/com/team27/pillmaxxer/dto/ManualFormRequest.java
@@ -1,5 +1,21 @@
 package com.team27.pillmaxxer.dto;
 
-public class ManualFormRequest {
+import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ManualFormRequest {
+    private String patientId;
+    private String medicationId;
+    private String medicationName;
+    private String dosage;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String instructions;
+    private String quantity;
+    private String frequency;
+    private boolean active;
 }
