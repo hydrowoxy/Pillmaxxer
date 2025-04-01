486764a86014f612db0fbac770bab511ca319ade
Ayush Patel <146563223+Ayushpatel2026@users.noreply.github.com>
2025-04-01 13:39:48 -0400

    connect drug interactions to med schedule (#25)

--- backend/src/main/java/com/team27/pillmaxxer/controller/DrugInteractionController_DDInter.java
+++ backend/src/main/java/com/team27/pillmaxxer/controller/DrugInteractionController_DDInter.java
@@ -1,32 +1,36 @@
 package com.team27.pillmaxxer.controller;
 
-
+import com.team27.pillmaxxer.dto.DrugInteractionResponse;
+import com.team27.pillmaxxer.model.MedicationSchedule;
 import com.team27.pillmaxxer.service.DrugInteractionService_DDInter;
+
+import lombok.extern.java.Log;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping("/api/ddi")
+@RequestMapping("/api/patients/{userId}/schedules/drug-interactions")
+@Log
 public class DrugInteractionController_DDInter {
 
-
     private final DrugInteractionService_DDInter ddiService;
-
 
     public DrugInteractionController_DDInter(DrugInteractionService_DDInter ddiService) {
         this.ddiService = ddiService;
     }
 
-
-    @GetMapping("/check")
-    public ResponseEntity<String> checkInteraction(
-            @RequestParam String drug1,
-            @RequestParam String drug2) {
-        return ddiService.getInteractionLevel(drug1, drug2)
-                .map(level -> ResponseEntity.ok("Interaction Severity: " + level))
-                .orElse(ResponseEntity.ok("No known interaction found."));
+    @PostMapping("/check")
+    public ResponseEntity<DrugInteractionResponse> checkInteraction(@RequestBody MedicationSchedule schedule) {
+        try {
+            log.info("Checking drug interactions for schedule: " + schedule.getId());
+            DrugInteractionResponse interactions = ddiService.getInteractionLevel(schedule);
+            return ResponseEntity.ok(interactions);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
-
